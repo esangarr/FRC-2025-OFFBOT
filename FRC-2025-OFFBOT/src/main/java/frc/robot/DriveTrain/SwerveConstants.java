01e84c1fabfd0f26f90ddfafd0f4f27cd3927ab1
esangarr <esangarr@hotmail.com>
2025-09-02 21:58:55 -0600

    Configurar IDs, encoders y offsets de los motores del swerve

--- FRC-2025-OFFBOT/src/main/java/frc/robot/DriveTrain/SwerveConstants.java
+++ FRC-2025-OFFBOT/src/main/java/frc/robot/DriveTrain/SwerveConstants.java
@@ -8,7 +8,7 @@ public class SwerveConstants {
 
     public static final double MAX_LINEAR_SPEED = Units.feetToMeters(19.0);
     public static final double TRACK_WIDTH_X = Units.inchesToMeters(28); 
-    public static final double TRACK_WIDTH_Y = Units.inchesToMeters(31.7); 
+    public static final double TRACK_WIDTH_Y = Units.inchesToMeters(32); 
     public static final double DRIVE_BASE_RADIUS =
         Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
     public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
@@ -29,37 +29,38 @@ public class SwerveConstants {
         Units.degreesToRadians(540),
         Units.degreesToRadians(720)
     );
-    
-    public static final class frontLeft{
-
-        public static final int DrivePort = 1; 
-        public static final int TurnPort = 2; 
-        public static final int EncPort = 19;
-        public static final double offset = 0.08;                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ; //48     //93  //138      //48 o 138 o 228
- 
-        public static final boolean DrivemotorReversed = true;
-        public static final boolean TurnmotorReversed = true;
-
-    }
 
     public static final class frontRight{
 
-        public static final int DrivePort = 4; 
-        public static final int TurnPort = 3; 
-        public static final int EncPort = 20; 
-        public static final double offset = -0.47; 
+        public static final int DrivePort = 1; 
+        public static final int TurnPort = 2; 
+        public static final int EncPort = 21; 
+        public static final double offset = -0.499; 
  
         public static final boolean DrivemotorReversed = true;
         public static final boolean TurnmotorReversed = true;
 
     }
+    
+    public static final class frontLeft{
+
+        public static final int DrivePort = 3; 
+        public static final int TurnPort = 4; 
+        public static final int EncPort = 22;
+        public static final double offset = -0.45;                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ; //48     //93  //138      //48 o 138 o 228
+ 
+        public static final boolean DrivemotorReversed = true;
+        public static final boolean TurnmotorReversed = true;
+
+    }
+
 
     public static final class backLeft{
 
         public static final int DrivePort = 5; 
         public static final int TurnPort = 6; 
-        public static final int EncPort = 21; 
-        public static final double offset = 0.49;
+        public static final int EncPort = 20; 
+        public static final double offset = -0.48;
  
         public static final boolean DrivemotorReversed = true;
         public static final boolean TurnmotorReversed = true; 
@@ -70,8 +71,8 @@ public class SwerveConstants {
 
         public static final int DrivePort = 7; 
         public static final int TurnPort = 8; 
-        public static final int EncPort = 22; 
-        public static final double offset = -0.43; 
+        public static final int EncPort = 19; 
+        public static final double offset = 0.09668; 
  
         public static final boolean DrivemotorReversed = true;
         public static final boolean TurnmotorReversed = true;
